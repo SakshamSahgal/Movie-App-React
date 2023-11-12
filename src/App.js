b4098c9448d54fb1646e5ae1d41438b4ea141632
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-11-12 14:13:10 +0530

    added loading skeleton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import List from './components/list';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 
 function App() {
 
--- src/components/list.js
+++ src/components/list.js
@@ -1,11 +1,13 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, CardBody, CardTitle, CardText, CardHeader } from 'reactstrap'; // Import
+import { Card, CardBody, CardHeader } from 'reactstrap'; // Import
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
 function List() {
     const [data, setData] = useState([]); //useState hook to initialize the state with an empty array
     useEffect(() => {
         axios.get('http://localhost:8080/testApi')
-            .then((res)=>{
+            .then((res) => {
                 console.log(res.data);
                 setData(res.data);
             });
@@ -13,16 +15,36 @@ function List() {
 
     return (
         <div>
-            <h1 className="mt-4 mb-4">Test Api</h1>
+            <h1 className="mt-4 mb-4 text-center">Test Api</h1>
             <div className="container text-center">
-                {data.map(item => (
-                    <Card key={item.name} className="mb-3">
-                        <CardHeader>{item.name}</CardHeader>
-                        <CardBody>
-                            <img src={`data:image/png;base64,${item.base64}`} alt={item.name} className="img-fluid" />
-                        </CardBody>
-                    </Card>
-                ))}
+
+                {data.length === 0 ? (
+                    // Display skeleton while data is being fetched
+                    <SkeletonTheme color="#f0f0f0" highlightColor="#d9d9d9">
+                    {[...Array(5)].map((_, index) => (
+                            <Card key={index} className="mb-3">
+                                <CardHeader>
+                                    <Skeleton height={30} />
+                                </CardHeader>
+                                <CardBody>
+                                    <Skeleton height={300} />
+                                </CardBody>
+                            </Card>
+                        ))}
+                        {/* Add more skeleton cards as needed */}
+                    </SkeletonTheme>
+                ) : (
+                    // Render cards with data once it is available
+                    data.map(item => (
+                        <Card key={item.name} className="mb-3">
+                            <CardHeader>{item.name}</CardHeader>
+                            <CardBody>
+                                <img src={`data:image/png;base64,${item.base64}`} alt={item.name} className="img-fluid" />
+                            </CardBody>
+                        </Card>
+                    ))
+                )}
+
             </div>
         </div>
     );
